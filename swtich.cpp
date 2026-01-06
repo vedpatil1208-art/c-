fe621f74e68aa148be08f8ad727c10aa25adbc0a
Ved <vedpatil1208@gmail.com>
2026-01-06 11:53:22 +0530

    Leap year operation

---
+++ swtich.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day;
+    cout << "Enter day number (1-7): ";
+    cin >> day;
+
+    switch (day) {
+        case 1:
+         cout << "Monday";
+         break;
+        case 2:
+         cout << "Tuesday";
+          break;
+         case 3:
+         cout << "Wednesday";  
+          break;
+         case 4:
+         cout << "Thursday";
+          break;
+         case 5:
+         cout << "Friday";
+         break;
+         case 6:
+         cout << "Saturday";
+
+    }
+}
