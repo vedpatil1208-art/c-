44ad6d31b7b125bafcdf33356dedf29fcd2281fd
Ved <vedpatil1208@gmail.com>
2026-01-12 10:08:06 +0530

    Fibono

---
+++ fibono.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    int a = 0, b = 1, c;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
