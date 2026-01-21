9879530a0476193d2e93a3f9f20c1a33d91a6212
Ved <vedpatil1208@gmail.com>
2026-01-21 10:53:11 +0530

    Reverse number

--- fibono.cpp
+++ fibono.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cout << "Enter number of terms: ";
-    cin >> n;
-
+void fibonacci(int n) {
     int a = 0, b = 1, c;
 
     cout << "Fibonacci Series: ";
-
     for (int i = 1; i <= n; i++) {
         cout << a << " ";
         c = a + b;
         a = b;
         b = c;
     }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    fibonacci(n);   // function call
 
     return 0;
 }
